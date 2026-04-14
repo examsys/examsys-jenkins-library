a40d31a715aaf2e19c03c9db43e2b9d6f5754945
Neill Magill <neill.magill@nottingham.ac.uk>
2026-04-14 13:56:08 +0100

    Use secure URL

--- vars/examsysInstallLanguagePacks.groovy
+++ vars/examsysInstallLanguagePacks.groovy
@@ -1,8 +1,10 @@
 def call(Map pipelineParams = [:]) {
     // The version branch of ExamSys we are building.
     String version = pipelineParams.version ?: 'develop'
+    String langServer = pipelineParams.langServer ?: 'https://examsys-oss.nottingham.ac.uk'
+    String langBasePath = pipelineParams.langBasePath ?: 'langpacks/'
 
-    String langPacksURL = 'http://examsys-oss.nottingham.ac.uk/langpacks/' + version + '/'
+    String langPacksURL = langServer + '/' + langBasePath + version + '/'
 
     // Download and install all language packs for ExamSys.
     sh '''if [ -f rogo.zip ]; then rm rogo.zip; fi'''
